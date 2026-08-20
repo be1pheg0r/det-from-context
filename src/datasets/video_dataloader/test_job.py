2cae92466e00c2855d2d4d97d029ec9211e0f2ba
mrdragar <yshhenyaev@mail.ru>
2026-08-20 13:15:28 +0300

    Fix formating

---
+++ src/datasets/video_dataloader/test_job.py
@@ -0,0 +1,25 @@
+import yaml
+from .dataloader import create_video_dataloader
+
+def main():
+    with open("config.yaml", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    
+    
+    dataloader = create_video_dataloader(config)
+    
+    
+    print("Количество samples:", len(dataloader.dataset))
+
+
+    for frames, targets in dataloader:
+        print("Frames shape:", frames.shape)
+        print("Number of targets:", len(targets))
+    
+        for target in targets:
+            print("boxes:", target["boxes"].shape, "labels:", target["labels"].shape)
+        print(target)
+        break
+
+if __name__ == "__main__":
+    main()
